ce1906ba2bde59dd869331ad17dd423b4807e81d
pc <ammar.futur@gmail.com>
2024-01-26 12:36:03 +0100

    tto Commit

---
+++ src/main/java/mypackage/myservlet.java
@@ -0,0 +1,57 @@
+package mypackage;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class myservlet
+ */
+
+@WebServlet("/srv")
+public class myservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public myservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+        PrintWriter writer = response.getWriter();
+		
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(userName.equals("admin") && password.equals("admin"))
+				{
+					writer.println("Valid Login , WELCOME");
+				}
+		else
+			{
+				writer.println("Invalid Login , Retry agin");
+			}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+	}
+
+}
